38a308a522d88c9b04eb575a1c5f5f0284fa5e6b
Pham Ngoc Linh <pnlinh1207@gmail.com>
2019-01-08 11:52:43 +0700

    HomeController test

--- tests/Feature/HomeControllerTest.php
+++ tests/Feature/HomeControllerTest.php
@@ -3,16 +3,17 @@
 namespace Tests\Feature;
 
 use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
-class ExampleTest extends TestCase
+class HomeControllerTest extends TestCase
 {
     /**
      * A basic test example.
      *
      * @return void
      */
-    public function testBasicTest()
+    public function testExample()
     {
         $response = $this->get('/');
 
