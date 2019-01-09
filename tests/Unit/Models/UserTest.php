58149b569d6a45cef93244947d7f2e32ca9a98cb
Pham Ngoc Linh <pnlinh1207@gmail.com>
2019-01-09 17:26:34 +0700

    Test model config

--- tests/Unit/Models/UserTest.php
+++ tests/Unit/Models/UserTest.php
@@ -10,32 +10,13 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 class UserTest extends TestCase
 {
     /** test */
-    public function test_contains_valid_fillable_properties()
-    {
-        $m = new User();
-
-        $this->assertEquals(['name', 'email', 'password'], $m->getFillable());
-    }
-
-    /** test */
-    public function test_contains_hidden_valid_properties()
-    {
-        $m = new User();
-
-        $this->assertEquals(['password', 'remember_token'], $m->getHidden());
-    }
-
-    public function test_valid_table_name()
-    {
-        $m = new User();
-
-        $this->assertEquals('users', $m->getTable());
-    }
-
-    public function test_valid_primary_key()
+    public function test_model_configuration()
     {
         $m = new User();
 
         $this->assertEquals('id', $m->getKeyName());
+        $this->assertEquals('users', $m->getTable());
+        $this->assertEquals(['password', 'remember_token'], $m->getHidden());
+        $this->assertEquals(['name', 'email', 'password'], $m->getFillable());
     }
 }
