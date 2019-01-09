2670ffc6e6e83930c96295a162faeaa0dc948ef7
Pham Ngoc Linh <pnlinh1207@gmail.com>
2019-01-09 17:24:19 +0700

    Update test user model

--- tests/Unit/Models/UserTest.php
+++ tests/Unit/Models/UserTest.php
@@ -16,4 +16,26 @@ class UserTest extends TestCase
 
         $this->assertEquals(['name', 'email', 'password'], $m->getFillable());
     }
+
+    /** test */
+    public function test_contains_hidden_valid_properties()
+    {
+        $m = new User();
+
+        $this->assertEquals(['password', 'remember_token'], $m->getHidden());
+    }
+
+    public function test_valid_table_name()
+    {
+        $m = new User();
+
+        $this->assertEquals('users', $m->getTable());
+    }
+
+    public function test_valid_primary_key()
+    {
+        $m = new User();
+
+        $this->assertEquals('id', $m->getKeyName());
+    }
 }
