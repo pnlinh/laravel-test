02ebc6a8112e9fa77fbfea6a0d73255fdbfc0b24
Ngoc Linh Pham <pnlinh1207@gmail.com>
2019-03-18 11:54:09 +0700

    About page test

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -10,4 +10,9 @@ class HomeController extends Controller
     {
         return view('welcome');
     }
+
+    public function about()
+    {
+        return 'About page';
+    }
 }
--- tests/Feature/HomeControllerTest.php
+++ tests/Feature/HomeControllerTest.php
@@ -8,15 +8,19 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class HomeControllerTest extends TestCase
 {
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-    public function testExample()
+    /** @test */
+    public function home_page()
     {
         $response = $this->get('/');
 
         $response->assertStatus(200);
+    }
+
+    /** @test */
+    public function about_route_return_something()
+    {
+        $response = $this->get('about');
+
+        $response->assertSee('About');
     }
 }
